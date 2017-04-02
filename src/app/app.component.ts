9e6307e6aa15fe865e68cd065942509535020b1f
sombochea <sombochea100@gmail.com>
2017-04-02 23:23:06 +0700

    Add testing with Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+  constructor(public af: AngularFire) {
+    this.items = af.database.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Anonymous,
+      method: AuthMethods.Anonymous
+    });
+  }
+
+  Send(desc: string) {
+    this.items.push({ messages: desc });
+    this.msgVal = '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBZ9Wd6jQLx48MmR6q97NKzujKymhIBEj4",
+    authDomain: "torrid-heat-2560.firebaseapp.com",
+    databaseURL: "https://torrid-heat-2560.firebaseio.com",
+    projectId: "torrid-heat-2560",
+    storageBucket: "torrid-heat-2560.appspot.com",
+    messagingSenderId: "256059146284"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
